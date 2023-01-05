da5bd63ef2cf05cd82630172e4915e4587ced403
Luiz Repiso <luizrepiso@yahoo.es>
2023-01-05 19:56:39 -0300

    DTO

--- backend/src/main/java/valeriatamarindo/loja/services/CategoryService.java
+++ backend/src/main/java/valeriatamarindo/loja/services/CategoryService.java
@@ -1,23 +1,28 @@
 package valeriatamarindo.loja.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import valeriatamarindo.loja.dto.CategoryDTO;
 import valeriatamarindo.loja.entities.Category;
 import valeriatamarindo.loja.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
-	@Transactional(readOnly=true)
-	public List<Category> findAll(){
-		return repository.findAll();
+
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
 	}
 
 }
