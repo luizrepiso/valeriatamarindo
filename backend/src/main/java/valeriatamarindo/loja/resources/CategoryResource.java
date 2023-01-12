4e26794ce73883c9d920ee7eae6331e95eef419b
Luiz Repiso <luizrepiso@yahoo.es>
2023-01-12 10:53:37 -0300

    Update Category

--- backend/src/main/java/valeriatamarindo/loja/resources/CategoryResource.java
+++ backend/src/main/java/valeriatamarindo/loja/resources/CategoryResource.java
@@ -1,5 +1,6 @@
 package valeriatamarindo.loja.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import valeriatamarindo.loja.dto.CategoryDTO;
 import valeriatamarindo.loja.services.CategoryService;
-
-import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -42,6 +42,13 @@ public class CategoryResource {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
+
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> insert(@PathVariable Long id, @RequestBody CategoryDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
 
 	}
 }
--- backend/src/main/java/valeriatamarindo/loja/services/CategoryService.java
+++ backend/src/main/java/valeriatamarindo/loja/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import valeriatamarindo.loja.dto.CategoryDTO;
 import valeriatamarindo.loja.entities.Category;
 import valeriatamarindo.loja.repositories.CategoryRepository;
-import valeriatamarindo.loja.services.exceptions.EntityNotFoundException;
+import valeriatamarindo.loja.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -43,4 +45,15 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 	}
 
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+	}
 }
