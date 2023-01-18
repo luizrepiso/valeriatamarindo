f45654ba0d2b3279d29391bbab821b69af5f8a8b
Luiz Repiso <luizrepiso@yahoo.es>
2023-01-17 22:15:52 -0300

    Product entity, SQL script

--- backend/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO tb_category(name, created_At) VALUES ('Blusas', NOW());
-INSERT INTO tb_category(name, created_At) VALUES ('Vestidos', NOW());
-INSERT INTO tb_category(name, created_At) VALUES ('Calças', NOW());
