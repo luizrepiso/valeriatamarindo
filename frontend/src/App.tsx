f68601b134e7117b507d4c5f36684725d38ac6ad
Luiz Repiso <luizrepiso@yahoo.es>
2023-07-20 18:52:57 -0300

    Navbar styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import './assets/styles/custom.scss';
-import "./App.css";
+import './App.css';
+import Navbar from 'components/Navbar';
 
-function App() {
-  
-  return <h1>Hello Valeria Tamarindo</h1>;
-}
+const App = () => {
+  return <Navbar />;
+};
+
 export default App;
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,46 @@
+import './styles.css';
+import '@popperjs/core';
+import 'bootstrap/js/dist/collapse';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-danger main-nav">
+      <div className="container-fluid">
+        <a href="link" className="nav-logo-text">
+          <h4>Loja Valeria Tamarindo</h4>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#valeriatamarindo-navbar"
+          aria-controls="valeriatamarindo-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="valeriatamarindo">
+          <ul className="navbar-nav offset-md-2 main-menu">
+            <li>
+              <a href="link" className="active">
+                HOME
+              </a>
+            </li>
+            <li>
+              <a href="link">CATÁLOGO</a>
+            </li>
+            <li>
+              <a href="link">ADMIN</a>
+            </li>
+            <li>
+              <a href="link">ORDER</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
