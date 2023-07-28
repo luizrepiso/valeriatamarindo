56174a0a372cdb6d59cb15abea929f8d24f08ba9
Luiz Repiso <luizrepiso@yahoo.es>
2023-07-27 21:50:38 -0300

    FormatPrice

--- frontend/src/components/ColectionPrice/index.tsx
+++ frontend/src/components/ColectionPrice/index.tsx
@@ -1,3 +1,4 @@
+import { formatPrice } from 'util/formaters';
 import './styles.css';
 
 type Props = {
@@ -8,7 +9,7 @@ const ColectionPrice = ({ price }: Props) => {
   return (
     <div className="colection-price-container">
       <span>R$</span>
-      <h3>{price}</h3>
+      <h3>{formatPrice(price)}</h3>
     </div>
   );
 };
