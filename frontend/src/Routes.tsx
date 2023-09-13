377ae79ea19f1ef019371855fc6ec7eef4487fc4
Luiz Repiso <luizrepiso@yahoo.es>
2023-09-13 10:02:43 -0300

    Auth routes

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -4,6 +4,7 @@ import Catalog from 'pages/Catalog';
 import ProductDetails from 'pages/ProductDetails';
 import Home from 'pages/Home';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Auth from 'pages/Admin/Auth';
 
 const Routes = () => (
   <BrowserRouter>
@@ -17,6 +18,11 @@ const Routes = () => (
       </Route>
       <Route path="/products/:productId">
         <ProductDetails />
+      </Route>
+      <Redirect from="/admin/auth" to="/admin/auth/login" exact />
+      <Route path="/admin"></Route>
+      <Route path="/admin/auth">
+        <Auth />
       </Route>
       <Redirect from="/admin" to="/admin/products" exact />
       <Route path="/admin">
---
+++ frontend/src/pages/Admin/Auth/index.tsx
@@ -0,0 +1,30 @@
+import LogoImg from 'assets/images/logo.png';
+import { Route, Switch } from 'react-router-dom';
+
+const Auth = () => {
+  return (
+    <div className="auth-container">
+      <div className="auth-banner-container">
+        <h1>Loja Valeria Tamarindo</h1>
+        <p>A melhor loja de roupas de Mirassolândia</p>
+        <div className="image-container">
+          <img className="main-image" src={LogoImg} alt="Modelo" />
+        </div>
+        <div className="auth-form-container">
+          <Switch>
+            <Route path="/admin/auth/login">
+              <h1>Card de login</h1>
+            </Route>
+            <Route path="/admin/auth/signup">
+              <h1>Card de signup</h1>
+            </Route>
+            <Route path="/admin/auth/recover">
+              <h1>Card de recover</h1>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Auth;
